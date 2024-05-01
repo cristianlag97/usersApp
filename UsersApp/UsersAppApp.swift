89b7922270f33bf3430d422680fa76b8bb02703b
Cristian Laguna <cristiandavidlaguna.97@gmail.com>
2024-05-01 17:54:33 -0500

    Initial Commit

---
+++ UsersApp/UsersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersAppApp.swift
+//  UsersApp
+//
+//  Created by Cristian David Laguna Aldana on 1/05/24.
+//
+
+import SwiftUI
+
+@main
+struct UsersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
